2fa6d5884a4967d629fbf6375a423ae7c9fe5613
Simon Rozet <simon@rozet.name>
2008-09-01 18:33:58 +0200

    started to write test/spec for Database. those will be unit test. I'll keep specs as integration tests

---
+++ test/database_test.rb
@@ -0,0 +1,71 @@
+require 'rubygems'
+require 'test/spec'
+require 'mocha'
+
+require File.dirname(__FILE__) + '/../lib/couch_rest'
+
+CouchHost = 'http://0.0.0.0:5984'
+TestDatabase = 'couchrest-test'
+
+
+describe 'Database' do
+  before(:each) do
+    @server = stub('server', :get => '', :post => '', :put => '', :delete => '')
+    @database = CouchRest::Database.new(@server, TestDatabase)
+  end
+
+  it 'has an accessor to the database name' do
+    @database.name.should.equal TestDatabase
+  end
+
+  it 'has an accessor to the server instance' do
+    @database.server.should.equal @server
+  end
+
+  describe 'Getting a list of documents' do
+    it 'GET name/_all_docs' do
+      @server.expects(:get).with(TestDatabase + '/_all_docs', {})
+      @database.documents
+    end
+
+    it 'passes params around if necessary' do
+      @server.expects(:get).with(TestDatabase + '/_all_docs', :startkey => 'somedoc', :count => 3)
+      @database.documents(:startkey => 'somedoc', :count => 3)
+    end
+  end
+
+  describe 'Creating a temporary view' do
+    it 'POST name/_temp_view with given fonction' do
+      @server.expects(:post).with(TestDatabase + '/_temp_view', anything, 'function(doc){emit(null, doc);}', anything)
+      @database.temp_view('function(doc){emit(null, doc);}')
+    end
+
+    it 'passes parameters around if necessary' do
+      @server.expects(:post).with(anything, {:startkey => 'foo'}, anything, anything)
+      @database.temp_view('foo', :startkey => 'foo')
+    end
+
+    it 'POST with application/json Content-Type' do
+      @server.expects(:post).with(anything, anything, anything, {'Content-Type' => 'application/json'})
+      @database.temp_view('foo')
+    end
+  end
+
+  describe 'Getting a view' do
+    it 'GET name/_view/view_name' do
+      @server.expects(:get).with("#{TestDatabase}/_view/my-view", {})
+      @database.view('my-view')
+    end
+
+    it 'passes parameters around if necessary' do
+      @server.expects(:get).with(anything, :count => 100)
+      @database.view('my-view', :count => 100)
+    end
+  end
+
+  describe 'Issuing a search' do
+  end
+
+  describe 'Issuing an action' do
+  end
+end
